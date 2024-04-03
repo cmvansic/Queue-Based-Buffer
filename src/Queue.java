b9ab237b27384530152a8038250fae29d21d30fb
Caden VanSickle <cmvansic@mtu.edu>
2024-04-02 22:56:16 -0400

    Made Queue.java implement QueueInterface

--- src/Queue.java
+++ src/Queue.java
@@ -1,2 +1,32 @@
-public class Queue {
+public class Queue implements QueueInterface {
+
+    @Override
+    public void enqueue(Object element) throws QueueFullException {
+
+    }
+
+    @Override
+    public Object dequeue() throws QueueEmptyException {
+        return null;
+    }
+
+    @Override
+    public Object peek() throws QueueEmptyException {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean isFull() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
 }
