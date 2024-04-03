797b737ed5daf1e090091745228d6d337b780134
Caden VanSickle <cmvansic@mtu.edu>
2024-04-02 23:28:54 -0400

    Started to write code to pull and read file from command line argument. Decided to stop here due to brain fog.

--- src/Program8.java
+++ src/Program8.java
@@ -1,4 +1,17 @@
-
+import java.io.File;
+import java.util.Scanner;
 public class Program8 {
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Provide a file name as a command line argument.");
+            return; // not neccesary?
+        }
+
+        File inputFile = new File(args[0]); // File in command line at first index
+        try (Scanner scan = new Scanner(inputFile)) {
+            scan.useDelimiter(""); //empty delimiter so the Input is read character by character.
+        }
+
+    }
 
 }
